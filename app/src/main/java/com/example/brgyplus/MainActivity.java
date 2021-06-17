3f526191e9b1e7bb30ada7bf0e6cde1fe852b21a
gekkkota <82653369+gekkkota@users.noreply.github.com>
2021-06-17 22:30:34 +0800

    User Creation

--- app/src/main/java/com/example/brgyplus/MainActivity.java
+++ app/src/main/java/com/example/brgyplus/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.brgyplus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private TextView signin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signin = (TextView) findViewById(R.id.signin);
+        signin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.signin:
+                startActivity(new Intent(this, SignIn.class));
+                break;
+        }
     }
 }
