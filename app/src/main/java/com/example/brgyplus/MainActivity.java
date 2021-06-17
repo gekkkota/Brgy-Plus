f10c8691e62f64232642ea462f1f18cc0904e6e3
Arvin Delos Reyes <amdelosreyes@iskolarngbayan.pup.edu.ph>
2021-06-18 02:25:57 +0800

    add splash screen

--- app/src/main/java/com/example/brgyplus/MainActivity.java
+++ app/src/main/java/com/example/brgyplus/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.brgyplus;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    int SPLASH_TIME = 3000; //This is 3 seconds
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //Code to start timer and take action after the timer ends
+        new Handler(getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Do any action here. Now we are moving to next page
+                Intent mySuperIntent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(mySuperIntent);
+
+                //This 'finish()' is for exiting the app when back button pressed from Home page which is ActivityHome
+                finish();
+
+            }
+        }, SPLASH_TIME);
     }
+
+
 }
