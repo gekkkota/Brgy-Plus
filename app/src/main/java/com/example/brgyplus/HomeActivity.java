f3d4f4a4cb42d308d44cab914bee041ed2690c22
Arvin Delos Reyes <amdelosreyes@iskolarngbayan.pup.edu.ph>
2021-06-18 20:37:06 +0800

    splash and homepage update

--- app/src/main/java/com/example/brgyplus/HomeActivity.java
+++ app/src/main/java/com/example/brgyplus/HomeActivity.java
@@ -3,6 +3,10 @@ package com.example.brgyplus;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        
+        ImageView leftIcon = findViewById(R.id.left_icon);
+        ImageView rightIcon = findViewById(R.id.right_icon);
+
+        leftIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(HomeActivity.this, "You Click in left Icon", Toast.LENGTH_SHORT).show();
+            }
+        });
+        rightIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(HomeActivity.this, "You Click in right Icon", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
